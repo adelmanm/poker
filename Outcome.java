5fcb92aa3937b7b84ad18ec3adeca05e35057679
danawr <danawr@gmail.com>
2016-08-23 08:50:20 +0300

    Delete Outcome.java

--- Outcome.java
+++
@@ -1,8 +0,0 @@
-/*
- *  Interface for Outcome. an Outcome can be the result of a chance event or a player action.
- *  Outcomes can be appended to History (game state) to produce a new future state. 
- */
-
-public interface Outcome {
-	int to_int(); //Outcome should be convertible to an array index 
-}
