208ce157105e0bb8adb277d01fc35ec1a330dc19
danawr <danawr@gmail.com>
2016-08-23 08:51:38 +0300

    Delete TerminalNode.java

--- TerminalNode.java
+++
@@ -1,7 +0,0 @@
-/*
- *  interface for game terminal nodes 
- */
-
-public interface TerminalNode extends History {
-	double get_utility(int player); //returns the utility/payoff of the terminal game state
-}
