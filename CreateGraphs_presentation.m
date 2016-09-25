3e9bc21c9223244dcacd79fe39a9742f17d31a4a
danawr <danawr@gmail.com>
2016-09-25 22:29:58 +0300

    Fixed Kuhn strategies to plot vs. visited nodes

--- CreateGraphs_presentation.m
+++ CreateGraphs_presentation.m
@@ -1,10 +1,15 @@
 clear all;
 close all;
 INFOSETS_PATH = '..\logs\infosets.csv';
-ALGORITHMS_PATH = '..\algorithms.csv';
+ALGORITHMS_PATH = '..\algorithms.xlsx';
 LOGS_PATH = '..\logs';
 %% plot informtion sets strategies - Kuhn
-ITERATION_GAP = 1;
+algorithm_num=1;
+[~,~,algorithms]=xlsread(ALGORITHMS_PATH);
+util_path = strcat(LOGS_PATH,'_', algorithms{algorithm_num}, '\util_hist.csv');
+[~,~,raw]=xlsread(util_path);
+visited_nodes=cell2mat(raw(:,4));
+INFOSETS_PATH=strcat(LOGS_PATH,'_', algorithms{algorithm_num}, '\infosets.csv');
 [~,~,raw]=xlsread(INFOSETS_PATH);
 infosets=raw(:,1);
 N = length(infosets);
@@ -12,41 +17,49 @@ N = length(infosets);
 infoset_legend={0};
 figure;
 hold on;
-title('Player 1 bet strategies');
-xlabel('iterations');
+cc= prism(N);
+title('Player 1 bet strategies', 'FontSize', 14);
+xlabel('Visited nodes');
 ylabel('strategy');
 for i = 1:N
     if isnumeric(infosets{i})
         infosets{i}=num2str(infosets{i});
     end
     strategy_path = strcat(LOGS_PATH,infosets{i},'_strategy.csv');
-    strategy_data = importdata(strategy_path);
-    iterations = (1:size(strategy_data,1)).*ITERATION_GAP;
+    strategy_path = strcat(LOGS_PATH,'_', algorithms{algorithm_num},'\', infosets(i),'_strategy.csv');
+    [~,~,raw]=xlsread(cell2mat(strategy_path)); 
+    strategy_data = cell2mat(raw);
     if length(infosets{i})==2
         infoset_legend{end+1}=infosets{i};
-        plot(iterations,strategy_data(:,2),'LineWidth',1,'Marker','.','MarkerSize',20);
+        plot(visited_nodes(1:length(strategy_data)),strategy_data(:,2),'LineWidth',1,'Marker','.', 'MarkerSize', 20, 'color', cc(i,:));
         drawnow;
     end
 end
 legend(infoset_legend{2:end});
+hold off;
 
 infoset_legend={0};
 figure;
 hold on;
-title('Player 0 bet strategies');
-xlabel('iterations');
+title('Player 0 bet strategies', 'FontSize', 14);
+xlabel('Visited nodes');
 ylabel('strategy');
 for i = 1:N
-    strategy_path = strcat(LOGS_PATH,infosets(i),'_strategy.csv');
-    strategy_data = importdata(strategy_path{1});
-    iterations = (1:size(strategy_data,1)).*ITERATION_GAP;
+    if isnumeric(infosets{i})
+        infosets{i}=num2str(infosets{i});
+    end
+    strategy_path = strcat(LOGS_PATH,infosets{i},'_strategy.csv');
+    strategy_path = strcat(LOGS_PATH,'_', algorithms{algorithm_num},'\', infosets(i),'_strategy.csv');
+    [~,~,raw]=xlsread(cell2mat(strategy_path)); 
+    strategy_data = cell2mat(raw);
     if length(infosets{i})~=2
         infoset_legend{end+1}=infosets{i};
-        plot(iterations,strategy_data(:,2),'LineWidth',1,'Marker','.','MarkerSize',20);
+        plot(visited_nodes(1:length(strategy_data)),strategy_data(:,1),'LineWidth',1,'Marker','.','MarkerSize', 20, 'color', cc(i,:));
         drawnow;
     end
 end
 legend(infoset_legend{2:end});
+hold off;
 %% plot player utilities 
 ITERATION_GAP = 1; % 1 for Kuhn, 100 for Leduc
 [~,~,algorithms]=xlsread(ALGORITHMS_PATH);
